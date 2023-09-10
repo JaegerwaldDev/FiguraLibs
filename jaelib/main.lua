bdaaaf5bdcd94e7d295ddc61bbe34c6db9721f63
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2023-09-11 01:15:10 +0200

    Add files via upload

---
+++ jaelib/main.lua
@@ -0,0 +1,26 @@
+--[[
+
+        __       _  __     _  _      ___              _____  _                     
+     __|  | ___ |_||  |   |_|| |_   |  _| ___  ___   |   __||_| ___  _ _  ___  ___ 
+    |  |  || .'|| ||  |__ | || . |  |  _|| . ||  _|  |   __|| || . || | ||  _|| .'|
+    |_____||__,||_||_____||_||___|  |_|  |___||_|    |__|   |_||_  ||___||_|  |__,|
+                                                               |___|               
+
+    Developed by https://www.github.com/JaegerwaldDev
+    GitHub @ https://www.github.com/JaegerwaldDev/FiguraLibs
+    Documentation @ https://github.com/JaegerwaldDev/FiguraLibs/blob/main/jd_cloud_checker.md
+
+    Copyright (c) JaegerwaldDev 2023
+
+    This library will keep getting developed as I make more stuff for Figura.
+    Wanna suggest something? Contact me: jaegerwald.dev@gmail.com
+    
+    :>
+
+]]--
+
+local jailib = {}
+
+jailib.hud = models.modules.jailib.hud.HUD
+
+return jailib
